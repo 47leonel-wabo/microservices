b092b5ee33e7b61742814cedbc99007313311721
leonel47 <waboleonel@gmail.com>
2023-12-14 00:49:35 +0100

    Order service handle and response error returned with the proper status code to client

--- order-service/src/main/java/com/wbt/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/wbt/orderservice/controller/OrderController.java
@@ -3,7 +3,11 @@ package com.wbt.orderservice.controller;
 import com.wbt.orderservice.dto.OrderRequestDto;
 import com.wbt.orderservice.dto.OrderResponseDto;
 import com.wbt.orderservice.service.OrderFulfillmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -12,8 +16,11 @@ import reactor.core.publisher.Mono;
 public record OrderController(OrderFulfillmentService orderService) {
 
     @PostMapping
-    public Mono<OrderResponseDto> processOrder(final @RequestBody Mono<OrderRequestDto> orderRequestDtoMono) {
-        return this.orderService.processOrder(orderRequestDtoMono);
+    public Mono<ResponseEntity<OrderResponseDto>> processOrder(final @RequestBody Mono<OrderRequestDto> orderRequestDtoMono) {
+        return this.orderService.processOrder(orderRequestDtoMono)
+                .map(ResponseEntity::ok)
+                .onErrorReturn(WebClientResponseException.class, ResponseEntity.badRequest().build())
+                .onErrorReturn(WebClientException.class, ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build());
     }
 
     @GetMapping(path = {"/users/{id}"})
